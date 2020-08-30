5872842f37f0cbc3ec0b0ec67660be8082281824
Nana Essandoh <nanaessandoh@gmail.com>
2020-08-30 19:01:36 -0230

    Chapter 19 Done

--- chapter_19/src/index.js
+++ chapter_19/src/index.js
@@ -1,4 +1,5 @@
 import {styleBody, addTitle, contact} from './dom';
+import users,{getPremUsers} from './data';
 
 console.log('Index file');
 addTitle("Test");
@@ -8,3 +9,9 @@ styleBody();
 addTitle('Hello, from the DOM file');
 
 addTitle(contact);
+
+
+const premUsers = getPremUsers(users);
+console.log(users, premUsers);
+
+console.log('Test 3');
