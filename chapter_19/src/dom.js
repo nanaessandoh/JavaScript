a4b7a29edc16cab33e8e0e2778dd8bfcc31750e3
Nana Essandoh <nanaessandoh@gmail.com>
2020-08-30 11:55:42 -0230

    Update README

---
+++ chapter_19/src/dom.js
@@ -0,0 +1,22 @@
+console.log('DOM file')
+
+
+const body  = document.querySelector('body');
+
+
+const styleBody = () => {
+
+    body.style.background = 'peachpuff';
+};
+
+const addTitle = (text) => {
+    const title = document.createElement('h1');
+    title.textContent = text;
+    body.appendChild(title);
+};
+
+
+const contact = 'mario@gmail.com';
+
+
+export {styleBody, addTitle,contact}
---
+++ chapter_19/src/index.js
@@ -0,0 +1,10 @@
+import {styleBody, addTitle, contact} from './dom';
+
+console.log('Index file');
+addTitle("Test");
+
+
+styleBody();
+addTitle('Hello, from the DOM file');
+
+addTitle(contact);
